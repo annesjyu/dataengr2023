cdea71d0abeccb8066e6577f1a13f5c75ee65426
Anne Yu <yu.ann.eml@gmail.com>
2024-05-21 22:50:23 +0800

    Update count_rows.py

--- count_rows.py
+++ count_rows.py
@@ -1,10 +1,7 @@
 from pyspark.sql import SparkSession
 
 def main():
-    spark = SparkSession.builder \
-        .master("spark://59e885f319c2:7077")  # Set the master URL here \
-        .appName("Spark Job to Count Records") \
-        .getOrCreate()
+    spark = SparkSession.builder.master("spark://59e885f319c2:7077").appName("Spark Job to Count Records").getOrCreate()
 
     # Example of loading data and performing operations
     df = spark.read.csv("Sales.csv", header=True, inferSchema=True)
